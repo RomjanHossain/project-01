b3896f45a04333fc159937dd60ee9ab7e749405f
root <root@localhost.localdomain>
2019-12-29 12:08:40 +0600

    gallery

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('gallery/', views.gallery, name='gallery'),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- web/views.py
+++ web/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def gallery(request):
+    return render(request, 'gallery/index.html')
